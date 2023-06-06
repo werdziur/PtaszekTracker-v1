0f35f985170e05826daf31d8307c35c99129ad6a
Weronika Dziua <wer.dziura@gmail.com>
2023-06-06 09:29:56 +0200

    fix: add semicolons that pretties removed

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,4 @@
-import * as model from './model.js'
+import * as model from './model.js';
 import {
 	renderResult,
 	searchResultsContainer,
@@ -31,110 +31,110 @@ import {
 	getCoords,
 	displayMapView,
 	showOverview,
-	clickOverview, removeAll
-} from './view.js'
+	clickOverview,
+	removeAll,
+} from './view.js';
 
-let userInput = document.querySelector('.search__input')
+let userInput = document.querySelector('.search__input');
 
-const closeResultsButton = document.querySelector('.search-results__close')
-const errorWindowText = document.querySelector('.error-window__text')
-const closeSearchButton = document.querySelector('.search__close')
-const buttonCloseAddWindow = document.querySelector('.add-bird__close')
-const navBar = document.querySelector('.navigation__items')
-const mainMapContainer = document.querySelector('.main-container')
+const closeResultsButton = document.querySelector('.search-results__close');
+const errorWindowText = document.querySelector('.error-window__text');
+const closeSearchButton = document.querySelector('.search__close');
+const buttonCloseAddWindow = document.querySelector('.add-bird__close');
+const navBar = document.querySelector('.navigation__items');
+const mainMapContainer = document.querySelector('.main-container');
 
 const renderList = async function () {
 	try {
-		await model.loadResult(userInput.value)
-		const birds = model.state.bird
+		await model.loadResult(userInput.value);
+		const birds = model.state.bird;
 		if (birds.length === 0) {
-			errorWindowText.innerText = 'No results. Please try again!'
-			errorMessage.style.display = 'flex'
+			errorWindowText.innerText = 'No results. Please try again!';
+			errorMessage.style.display = 'flex';
 		}
 		if (userInput.value === '') {
-			errorWindowText.innerText = 'This field cannot be empty!'
-			errorMessage.style.display = 'flex'
+			errorWindowText.innerText = 'This field cannot be empty!';
+			errorMessage.style.display = 'flex';
 		} else {
-			birds.forEach(el => renderResult(el))
+			birds.forEach(el => renderResult(el));
 		}
-		userInput.value = ''
+		userInput.value = '';
 	} catch (err) {
-		console.log(err)
-		throw err
+		console.log(err);
+		throw err;
 	}
-}
+};
 
 const updateObservationsList = function (birdName, layer) {
 	//filter for a bird
-	const [bird] = model.state.bird.filter(el => el.name === birdName.name)
-	bird.date = new Date()
-	bird.id = Math.random() + ''
-	bird.coords = getCoords()
+	const [bird] = model.state.bird.filter(el => el.name === birdName.name);
+	bird.date = new Date();
+	bird.id = Math.random() + '';
+	bird.coords = getCoords();
 	//take marker data and render marker
-	layer = renderMarker(bird.name, bird.date)
+	layer = renderMarker(bird.name, bird.date);
 
 	//update state and render bird on the obersvation list
-	model.addChosenBirdToObservations(bird, layer)
+	model.addChosenBirdToObservations(bird, layer);
 	model.state.observations.forEach(el => {
-		renderSelectedBird(el)
-	})
-}
+		renderSelectedBird(el);
+	});
+};
 
 const removeBirdElement = function (bird) {
-	model.removeBird(bird.id)
-}
+	model.removeBird(bird.id);
+};
 
 const getInfoAboutBird = function (birdName) {
-	const [bird] = model.state.bird.filter(el => el.name === birdName.name)
-	renderMoreInformation(bird)
-}
+	const [bird] = model.state.bird.filter(el => el.name === birdName.name);
+	renderMoreInformation(bird);
+};
 
 const findBirdToShow = function (birdsId) {
-	const [birdId] = model.state.observations.filter(el => el.id === birdsId.id)
-	displayMapView(birdId.coords)
-}
+	const [birdId] = model.state.observations.filter(el => el.id === birdsId.id);
+	displayMapView(birdId.coords);
+};
 
 const overview = function () {
-	if (model.state.observations.length === 0) return
-	const coords = model.state.observations.map(el => el.coords)
-	const langtitudes = coords.map(el => el[0])
-	const longtitudes = coords.map(el => el[1])
-	showOverview(langtitudes, longtitudes)
-	
-}
+	if (model.state.observations.length === 0) return;
+	const coords = model.state.observations.map(el => el.coords);
+	const langtitudes = coords.map(el => el[0]);
+	const longtitudes = coords.map(el => el[1]);
+	showOverview(langtitudes, longtitudes);
+};
 
-const removeAllObservations = function() {
-	model.removeAllElements()
-}
+const removeAllObservations = function () {
+	model.removeAllElements();
+};
 
 const init = function () {
-	removeAll(removeAllObservations)
-	clickOverview(overview)
-	showOnTheMap(findBirdToShow)
-	showMoreInformation(getInfoAboutBird)
-	getBirdToBeRemoved(removeBirdElement)
-	addSelectedBird(updateObservationsList)
-	model.getUserPosition(showPosition)
-	showBurgerButon(navBar)
-	closeWindow(closeSearchButton, searchWindow)
-	closeWindow(overlay, searchWindow)
-	closeWindow(overlay, successMessage)
-	closeWindow(buttonCloseAddWindow, addBirdWindow)
-	closeWindow(overlay, addBirdWindow)
-	closeWindow(buttonSearchSubmit, searchWindow)
-	closeWindow(overlay, mainContainerResults)
-	closeWindow(overlay, errorMessage)
-	closeWindow(closeResultsButton, mainContainerResults)
-	closeWindow(overlay, removeMessage)
-	displayAddWindow()
-	searchBirds(renderList)
-	showErrorWindow()
-	closeListOfObservations()
-	showObservationList(navBar)
-	closeMoreInfo()
-}
+	removeAll(removeAllObservations);
+	clickOverview(overview);
+	showOnTheMap(findBirdToShow);
+	showMoreInformation(getInfoAboutBird);
+	getBirdToBeRemoved(removeBirdElement);
+	addSelectedBird(updateObservationsList);
+	model.getUserPosition(showPosition);
+	showBurgerButon(navBar);
+	closeWindow(closeSearchButton, searchWindow);
+	closeWindow(overlay, searchWindow);
+	closeWindow(overlay, successMessage);
+	closeWindow(buttonCloseAddWindow, addBirdWindow);
+	closeWindow(overlay, addBirdWindow);
+	closeWindow(buttonSearchSubmit, searchWindow);
+	closeWindow(overlay, mainContainerResults);
+	closeWindow(overlay, errorMessage);
+	closeWindow(closeResultsButton, mainContainerResults);
+	closeWindow(overlay, removeMessage);
+	displayAddWindow();
+	searchBirds(renderList);
+	showErrorWindow();
+	closeListOfObservations();
+	showObservationList(navBar);
+	closeMoreInfo();
+};
 
-init()
+init();
 
 //
 
--- src/js/model.js
+++ src/js/model.js
@@ -2,43 +2,43 @@ export const state = {
 	bird: {},
 	observations: [],
 	markers: [],
-}
+};
 
 export const getUserPosition = function (showMap) {
 	if (navigator.geolocation)
 		navigator.geolocation.getCurrentPosition(showMap, function () {
-			alert('Could not get your position. Try again!')
-		})
-}
+			alert('Could not get your position. Try again!');
+		});
+};
 
 export const loadResult = async function (userInput) {
 	try {
-		const response = await fetch('birds.json')
-		const data = await response.json()
+		const response = await fetch('birds.json');
+		const data = await response.json();
 		state.bird = data.filter(el => {
-			return el.name.slice(0, userInput.length) === userInput.toLowerCase()
-		})
+			return el.name.slice(0, userInput.length) === userInput.toLowerCase();
+		});
 	} catch (error) {
-		console.error(`${error} :(!!`)
+		console.error(`${error} :(!!`);
 	}
-}
+};
 
 export const addChosenBirdToObservations = function (bird, marker) {
-	state.observations.push(bird)
-	state.markers.push(marker)
-	state.bird = {}
-}
+	state.observations.push(bird);
+	state.markers.push(marker);
+	state.bird = {};
+};
 
 export const removeBird = function (id) {
 	//bird
-	const birdIndex = state.observations.findIndex(el => el.id === id)
-	state.observations.splice(birdIndex, 1)
+	const birdIndex = state.observations.findIndex(el => el.id === id);
+	state.observations.splice(birdIndex, 1);
 	//marker
-	state.markers[birdIndex].remove()
-	state.markers.splice(birdIndex, 1)
-}
+	state.markers[birdIndex].remove();
+	state.markers.splice(birdIndex, 1);
+};
 
 export const removeAllElements = function () {
-	state.markers.forEach(e => e.remove())
-	state.observations = []
-}
+	state.markers.forEach(e => e.remove());
+	state.observations = [];
+};
